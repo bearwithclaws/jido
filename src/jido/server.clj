a25d77bb19b2c7d4ebee46bc7674fd0e3b40398d
Lim Cheng Soon <foress@gmail.com>
2013-07-07 02:26:27 +0800

    auto unshorten shortened links

--- src/jido/server.clj
+++ src/jido/server.clj
@@ -1,6 +1,8 @@
 (ns jido.server
  (:use compojure.core
         [clojure.tools.logging :only [info debug warn error]]
+        ;; url
+        [clojurewerkz.urly.core :only (host-of url-like)]
         ;; twitter
         [twitter.oauth]
         [twitter.callbacks]
@@ -42,6 +44,7 @@
         (text-area "tweeter")
         [:br]
         (submit-button {:class "btn"} "Get Links!"))
+      
       [:hr]
       [:ul (for [link links] (when link [:li (str "<a href='" link "'>" link "</a>")]))]]
      (include-js "//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js")
@@ -54,15 +57,33 @@
                                 (System/getenv "TWITTER_CONSUMER_SECRET")
                                 (System/getenv "TWITTER_CONSUMER_KEY")))
 
+(defn final-url [initial-url]
+  "Unshorten a URL"
+  (def url (client/get initial-url))
+  (last (:trace-redirects url)))
+
+(defn is-url-shortener? [initial-url]
+  "Check whether the URL has been shortened"
+  (let [hostname (host-of (url-like initial-url))]
+    (= (re-matches #"bit.ly|ow.ly|j.mp" hostname) hostname)))
+
+(defn check-url [initial-url]
+  "If the URL has been shortened, unshorten it"
+  (if (is-url-shortener? initial-url) (final-url initial-url) initial-url))
+
 (defn get-tweets [tweeter]
+  "Get tweets JSON from tweeter"
   (:body (statuses-user-timeline
              :oauth-creds my-creds
              :params {:screen-name tweeter})))
 
 (defn get-links [tweeter]
-  (vec (map :expanded_url (map first (map :urls (map :entities (get-tweets tweeter)))))))
+  "Get and process/unshorten list of links from tweets JSON"
+  (def raw-links (vec (map :expanded_url (map first (map :urls (map :entities (get-tweets tweeter)))))))
+  (vec (for [raw-link raw-links] (when raw-link (check-url raw-link)))))
 
 (defn list-links [tweeter]
+  "Put list of links of a tweeter into HTML template"
   (when-not (str/blank? tweeter)
    (def links (get-links [tweeter])))
   (index links))
